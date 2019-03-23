030d2386bf960b1a47c3d64e357b1b9815ec70e3
lidiatodoran <todoranelena@yahoo.com>
2019-03-23 14:07:09 +0200

    Teste

--- src/test/java/tlir2204MV/controller/TestIntrebare.java
+++ src/test/java/tlir2204MV/controller/TestIntrebare.java
@@ -31,24 +31,55 @@ public class TestIntrebare {
     }
 
     @Test
-    public void test2() throws InputValidationFailedException, DuplicateIntrebareException {
-        Intrebare intrebare = new Intrebare("enunt test?", "1) Varianta 1", "2) Varianta 2", "3) Varianta 3", "1", "Domeniu");
-        appController.addNewIntrebare(intrebare, file);
-        assertEquals(1, appController.getIntrebari().size());
+    public void test2(){
+
+        Intrebare intrebare = null;
+        try {
+            intrebare = new Intrebare("enunt test?", "1) Varianta 1", "2) Varianta 2", "3) Varianta 3", "1", "Domeniu");
+            try {
+                appController.addNewIntrebare(intrebare, file);
+            } catch (DuplicateIntrebareException e) {
+                System.out.println(e.getMessage());
+            }
+        } catch (InputValidationFailedException e) {
+            System.out.println(e.getMessage());
+        }
+
+        assertEquals(0, appController.getIntrebari().size());
     }
 
     @Test
-    public void test3() throws InputValidationFailedException, DuplicateIntrebareException {
-        Intrebare intrebare = new Intrebare("Enunt test?", "a) Varianta 1", "2) Varianta 2", "3) Varianta 3", "1", "Domeniu");
-        appController.addNewIntrebare(intrebare, file);
-        assertEquals(1, appController.getIntrebari().size());
+    public void test3() {
+        Intrebare intrebare = null;
+        try {
+            intrebare = new Intrebare("Enunt test?", "a) Varianta 1", "2) Varianta 2", "3) Varianta 3", "1", "Domeniu");
+            try {
+                appController.addNewIntrebare(intrebare, file);
+            } catch (DuplicateIntrebareException e) {
+                System.out.println(e.getMessage());
+            }
+        } catch (InputValidationFailedException e) {
+            System.out.println(e.getMessage());
+        }
+
+        assertEquals(0, appController.getIntrebari().size());
     }
 
     @Test
-    public void test4() throws InputValidationFailedException, DuplicateIntrebareException {
-        Intrebare intrebare = new Intrebare("A", "1) Varianta 1", "2) Varianta 2", "3) Varianta 3", "1", "Domeniu");
-        appController.addNewIntrebare(intrebare, file);
-        assertEquals(1, appController.getIntrebari().size());
+    public void test4() {
+        Intrebare intrebare = null;
+        try {
+            intrebare = new Intrebare("A", "1) Varianta 1", "2) Varianta 2", "3) Varianta 3", "1", "Domeniu");
+            try {
+                appController.addNewIntrebare(intrebare, file);
+            } catch (DuplicateIntrebareException e) {
+                System.out.println(e.getMessage());
+            }
+        } catch (InputValidationFailedException e) {
+            System.out.println(e.getMessage());
+        }
+
+        assertEquals(0, appController.getIntrebari().size());
     }
 
     @Test
